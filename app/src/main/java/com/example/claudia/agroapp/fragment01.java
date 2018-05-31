b1812d99adc77df1a90c5849445b26023b114adc
geovanniayala <38696905+geovanniayala@users.noreply.github.com>
2018-05-31 00:23:28 -0500

    fragment perfil de menu

---
+++ app/src/main/java/com/example/claudia/agroapp/fragment01.java
@@ -0,0 +1,24 @@
+package com.example.claudia.agroapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class fragment01 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment01, container, false);
+    }
+
+
+}
